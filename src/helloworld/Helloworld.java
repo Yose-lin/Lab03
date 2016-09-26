86cbb35e283af35b56e02d1750c651988136bf24
gb591012 <gb591012@C42146.ens.uqam.ca>
2016-09-26 10:09:51 -0400

    Ajout de Bye cruel world

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -17,6 +17,8 @@ public class Helloworld {
     public static void main(String[] args) {
         
         System.out.println("Hello World!");
+        
+        System.out.println("Bye cruel World!");
         // TODO code application logic here
     }
     
